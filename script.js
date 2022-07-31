63fb1dcc6397b62ff674f6c1fe2bb51ad39a1181
Daniel Popovici <dpopovici601@gmail.com>
2022-07-31 15:19:21 -0400

    Finished styling for calculator.

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function add(num1,num2) {
-    return num1 + num2
+    return num1 + num2;
 }
 function subtract(num1,num2) {
-    return num1 - num2
+    return num1 - num2;
 }
 function multiply(num1,num2) {
-    return num1 * num2
+    return num1 * num2;
 }
 function divide(num1,num2) {
-    return num1 / num2
+    return num1 / num2;
 }
 function sqroot(num1) {
-    return Math.sqrt(num1)
+    return Math.sqrt(num1);
+}
+
+function operate(operator,num1,num2){
+    add(num1,num2);
 }
