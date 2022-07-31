b96443439ee0595aebca8a36ad0bd94570f0af60
Daniel Popovici <dpopovici601@gmail.com>
2022-07-31 13:52:58 -0400

    created basic operator functions (add,subtract,multiply,divide,sqroot)

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(num1,num2) {
+    return num1 + num2
+}
+function subtract(num1,num2) {
+    return num1 - num2
+}
+function multiply(num1,num2) {
+    return num1 * num2
+}
+function divide(num1,num2) {
+    return num1 / num2
+}
+function sqroot(num1) {
+    return Math.sqrt(num1)
+}
