bd5b4c32bbccffc2814a9268f3823b50628e7d18
Daniel Popovici <dpopovici601@gmail.com>
2022-07-31 20:25:53 -0400

    all buttons except negative now display on the screen.

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ function sqroot(num1) {
 function operate(operator,num1,num2){
     add(num1,num2);
 }
+
+function displayScreen(text){
+    displaySpan.textContent += text;
+}
+
+function clearScreen(){
+    displaySpan.textContent='';
+}
+
+let displaySpan = document.querySelector('#display span');
+
+//Numbers Event Listeners
+
+let numbers = document.querySelectorAll('#numContainer button.display');
+numbers.forEach(number => number.addEventListener('click',function getText(){
+    let text = number.textContent;
+    displayScreen(text);
+}));
+
+//Operators EventListeners
+
+let operators = document.querySelectorAll('#operators button.display');
+operators.forEach(operator => operator.addEventListener('click',function getText(){
+    let text = operator.textContent;
+    displayScreen(text);
+}));
+
+
+let clear = document.querySelector('#clr');
+clear.addEventListener('click',()=> clearScreen());
+
+
+
